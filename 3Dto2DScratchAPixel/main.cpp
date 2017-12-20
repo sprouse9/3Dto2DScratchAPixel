8fa6722825ac93687925334f533320d899993046
randy sprouse <randysprouse@randys-imac-2.local>
2017-12-19 20:09:20 -0500

    Initial Commit

---
+++ 3Dto2DScratchAPixel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3Dto2DScratchAPixel
+//
+//  Created by randy sprouse on 12/19/17.
+//  Copyright © 2017 randy sprouse. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
